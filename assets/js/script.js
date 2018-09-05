e275cf78e7ef0489a36aed9ac78c1e47b742aca5
Umpoy <iumpoy@yahoo.com>
2018-09-05 11:44:38 -0700

    user can now select numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,23 @@ $(document).ready(initialize);
 
 function initialize() {
     create_board();
+    assign_click_handler();
 }
 
 function create_board() {
     for (var i = 1; i < 81; i++) {
         $('<div>').addClass('box').html(i).appendTo('.game')
+    }
+}
+
+function assign_click_handler() {
+    $('.box').on('click', select_number);
+}
+
+function select_number() {
+    if ($(this).hasClass('selected')) {
+        $(this).removeClass('selected')
+    } else {
+        $(this).addClass('selected')
     }
 }
