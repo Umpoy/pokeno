e6e382ba91292404a396e94143d5b9606b983b57
Umpoy <iumpoy@yahoo.com>
2018-09-05 11:38:09 -0700

    keno board created dynamically

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
-alert();
+$(document).ready(initialize);
+
+function initialize() {
+    create_board();
+}
+
+function create_board() {
+    for (var i = 1; i < 81; i++) {
+        $('<div>').addClass('box').html(i).appendTo('.game')
+    }
+}
