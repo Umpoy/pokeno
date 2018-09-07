21fa8fed18a9f97ee1f60701b3676c8a05f1468c
Umpoy <iumpoy@yahoo.com>
2018-09-06 18:54:01 -0700

    start of prize winnings

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 let user_count = 0
 let user_numbers = [];
+let user_money = 10
 
 $(document).ready(initialize);
 
@@ -36,7 +37,13 @@ function picked_number() {
 }
 
 function generate_winning_numbers() {
+    if (user_money == 0 || user_numbers.length == 0) {
+        return
+    }
+    user_money--
+    console.log(user_money)
     $('.box').off("click");
+    //$('.play').off("click");
     $('.box').removeClass('selected');
     let winning_numbers = [];
     while (winning_numbers.length < 20) {
@@ -45,17 +52,45 @@ function generate_winning_numbers() {
             winning_numbers.push(hold);
         }
     }
+    console.log(winning_numbers)
+
     select_winning_numbers(winning_numbers);
+
     //console.log(winning_numbers.sort(function (a, b) { return a - b }))
 }
 
 function select_winning_numbers(array) {
+
+    const hold_array = array.slice();
     const interval = setInterval(function () {
-        if (array.length === 0) {
+        if (hold_array.length === 0) {
             clearInterval(interval);
+            check_winnings(array);
             return
         }
-        $('.box:nth-child(' + array[0] + ')').addClass('selected')
-        array.shift();
-    }, 250)
+        $('.box:nth-child(' + hold_array[0] + ')').addClass('selected')
+        hold_array.shift();
+    }, 100)
+
+}
+
+function check_winnings(array) {
+
+    let correct_match = 0;
+    for (let i = 0; i < user_numbers.length; i++) {
+        if (array.includes(parseInt(user_numbers[i]))) {
+            correct_match++
+        }
+    }
+    if (correct_match === 0) {
+        return
+    }
+
+    switch (user_numbers.length) {
+        case 1:
+            if (correct_match == 1) {
+                user_money += 3
+            }
+    }
+    console.log(user_money)
 }
